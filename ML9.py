18d3aa220b15965955fc9425b316ac2ed2b5826c
ambadileelakal@gmail.com <ambadileelakal@gmail.com>
2023-02-13 14:35:49 +0530

    msg

--- ML9.py
+++ ML9.py
@@ -12,6 +12,17 @@ import numpy as np
 mydata = mypd.read_csv("/home/nasc/Documents/G/ML/doc/Wine.csv")
 print (mydata)
 
+"""
+#pairploting
+mysb.pairplot(mydata,hue ='quality')
+myplot.show()
+"""
+
+mydata.isnull().sum()
+for col in mydata.columns:
+    if mydata[col].isnull().sum() > 0:
+        mydata[col] = mydata[col].fillna(mydata[col].mean())
+print(mydata.isnull().sum().sum())
 
 x = mydata.iloc[:,9:11]
 print(x)
@@ -19,12 +30,45 @@ print(x)
 y = mydata.iloc[:,12:13]
 print(y)
 
-"""
-#pairploting
-mysb.pairplot(mydata,hue ='quality')
+mydata.hist(bins = 20, figsize = (10,10))
 myplot.show()
-"""
+
+myplot.bar(mydata['quality'],mydata['alcohol'])
+myplot.xlabel('quality')
+myplot.ylabel('alcohol')
+myplot.show()
+
+#correlation finding
+
+myplot.figure(figsize = (12,12))
+mysb.heatmap(mydata.corr(), annot = True)
+myplot.show()
+
+mydata.replace({'white' : 1,'red' : 0},inplace =True)
+mydata['best quality'] = mydata.quality.apply(lambda x:1 if x > 5 else 0)
+print(mydata['best quality'].value_counts())
+
+from sklearn.model_selection import train_test_split
+features = mydata.drop(['quality','best quality'],axis = 1)
+target = mydata['best quality']
+xtrain,xtest,ytrain,ytest = train_test_split(features, target, test_size = 0.2, random_state = 40, shuffle=True)
+xtrain.shape,xtest.shape
 
 mymodel = LogisticRegression()
-mymodel = mymodel.fit(x,y)
+mymodel = mymodel.fit(xtrain,ytrain)
 print(mymodel)
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+ypred = mymodel.predict(xtest)
+model_acc = accuracy_score(ypred,ytest)
+round(model_acc*100,2)
+
+mymetrix = confusion_matrix(ypred,ytest)
+print(mymetrix)
+
+print(classification_report(ytest,ypred))
+
+
+
+
